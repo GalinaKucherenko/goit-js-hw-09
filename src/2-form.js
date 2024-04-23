869a07365daec45c922d28ceddd84bf6bf9c05da
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-04-23 13:39:47 +0300

    Change2

---
+++ src/2-form.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+    
+// Під час завантаження сторінки перевіряю стан сховища
+const storedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (storedState) {
+      emailInput.value = storedState.email || '';
+      messageInput.value = storedState.message || '';
+}
+    
+ // Відстежуємо введення користувача та зберігаємо дані в сховищі
+form.addEventListener('input', function(event) {
+    const currentState = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+});
+
+// Подія відправки форми
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Забороняємо браузеру відправляти форму
+    const currentState = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(currentState); // Виводимо дані в консоль
+    localStorage.removeItem('feedback-form-state'); // Очищаємо сховище
+    emailInput.value = ''; // Очищаємо поле email
+    messageInput.value = ''; // Очищаємо поле повідомлення
+});
